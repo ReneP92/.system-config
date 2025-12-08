7fccb26f64dee7832433c01e24100de11a73fcf0
ReneP92 <rene.persau@gmail.com>
2025-12-08 08:46:43 +0000

    interim commit

--- root/.wezterm.lua
+++ root/.wezterm.lua
@@ -1,1 +1,39 @@
-/Users/renepersau/.wezterm.lua
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 14
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 1.0
+config.macos_window_background_blur = 10
+
+-- config.color_scheme = 'tokyonight_night'
+
+-- coolnight colorscheme:
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+-- Appearance
+config.cursor_blink_rate = 0
+config.hide_tab_bar_if_only_one_tab = true
+config.macos_window_background_blur = 40
+
+-- and finally, return the configuration to wezterm
+return config
