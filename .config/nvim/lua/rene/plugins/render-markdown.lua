68b61954bab576e90b520e7c4b0cc6995dfa4bff
ReneP92 <rene.persau@gmail.com>
2026-07-20 09:24:28 -0700

    add render-markdown plugin

---
+++ .config/nvim/lua/rene/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = "markdown",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    -- no latex parser or converter (utftex/latex2text) installed
+    latex = { enabled = false },
+  },
+}
